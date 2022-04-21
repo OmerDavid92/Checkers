d8bff282f9b7157d4efc92dbc94fe0deb82e5be0
Omer David <omerdavid321@gmail.com>
2022-04-21 17:31:18 +0300

    isMoovingUp

--- B22 Ex01 OmerDavid 308483437 RazielAlron 316061415/Player.cs
+++ B22 Ex01 OmerDavid 308483437 RazielAlron 316061415/Player.cs
@@ -11,12 +11,14 @@ namespace Checkers
         public string m_PlayerName { get; }
         public ToolSign m_ToolSign { get; }
         public bool m_IsHuman { get; } //To check if private is needed
+        public bool m_IsMoovingUp { get; }
 
-        public Player(char i_TrooperSign, char i_kingSign, bool i_IsHuman, string i_PlayerName = "")
+        public Player(char i_TrooperSign, char i_kingSign, bool i_IsHuman, bool i_IsMoovingUp, string i_PlayerName = "")
         {
             m_PlayerName = i_PlayerName;
             m_ToolSign = new ToolSign(i_TrooperSign, i_kingSign);
             m_IsHuman = i_IsHuman;
+            m_IsMoovingUp = i_IsMoovingUp;
         }
     }
 }
